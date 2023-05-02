980b1bbfd9bbd5522067d05bc67e1260638b7204
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-05-02 10:36:16 +0600

    array map used done in here

--- js/countries.js
+++ js/countries.js
@@ -4,6 +4,30 @@ const loadCountries = () => {
         .then(data => displayCountries(data))
 }
 const displayCountries = countries => {
-    console.log(countries);
+    console.log(countries[0]);
+    const countriesHtml = countries.map(country => getcountryHtml(country));
+    // console.log(countriesHtml);
+    const container = document.getElementById('countries');
+
+    //show countries in container
+    // container.innerHTML = countriesHtml
+
+    //to remove comma from array
+    const arrayCommaremover = countriesHtml.join(' ');
+    container.innerHTML = arrayCommaremover;
+
+
+
+}
+const getcountryHtml = country => {
+    const countryhtmldiv = `
+    <div class="countrydiv">
+    <h2>
+    ${country.name.common}
+    </h2>
+    <img src="${country.flags.png}">
+    </div>
+    `
+    return countryhtmldiv;
 }
 loadCountries();
